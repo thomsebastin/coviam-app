848c9ba1e7eb886575ec23f8c42a993a32ada410
Thomas Sebastian <thomas@inkoniq.com>
2016-06-22 08:12:21 +0530

    added hide functionality to 'add to cart' btn

--- modules/controller.js
+++ modules/controller.js
@@ -1,5 +1,12 @@
 var cvController = angular.module('cvApp.controller', []);
 
-cvController.controller('cvCtrl', function() {
+cvController.controller('cvCtrl', function($scope) {
   console.log("Controller inititated!!");
+  $scope.isHidden = false;
+  $scope.isShown = false;
+  //function to hide cart button
+  $scope.hideCartBtn = function() {
+    $scope.isHidden = true;
+    $scope.isShown = true;
+  }
 });
