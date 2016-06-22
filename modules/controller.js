4177e210b4bbc302fdbf01f76cbf9bb21d9f6d97
Thomas Sebastian <thomas@inkoniq.com>
2016-06-22 23:56:59 +0530

    Added functionality for updating items with quantity and price

--- modules/controller.js
+++ modules/controller.js
@@ -4,6 +4,7 @@ cvController.controller('cvCtrl', function($scope, GlobalService) {
   console.log("Controller inititated!!");
   
   $scope.itemsInCart = 0;
+  $scope.allGoods = [];
 
   // calling API for getting products
   $scope.getProductDetails = function() {
@@ -18,12 +19,22 @@ cvController.controller('cvCtrl', function($scope, GlobalService) {
     product.isHidden = true;
     $scope.itemsInCart += 1;
     product.totalSelectedItems += 1;
+
+    $scope.allGoods.push({
+      name: product.brand_name,
+      quantity: product.totalSelectedItems,
+      total_amt: product.totalSelectedItems * product.price
+    }); 
   };
 
   // add one more item to the total items on clicking plus
   $scope.addOneMoreItem = function(product) {
     $scope.itemsInCart += 1;
     product.totalSelectedItems += 1;
+
+    var found = ($scope.allGoods).filter(function(item) { return item.name === product.brand_name; });
+    found[0].quantity += 1;
+    found[0].total_amt = found[0].quantity * product.price;
   };
 
   // remove one item from cart on clicking '-'
@@ -31,10 +42,15 @@ cvController.controller('cvCtrl', function($scope, GlobalService) {
     if ($scope.itemsInCart > 0) {
       $scope.itemsInCart -= 1;
       (product.totalSelectedItems > 0) ? (product.totalSelectedItems -= 1) : product.totalSelectedItems;
+
+      var found = ($scope.allGoods).filter(function(item) { return item.name === product.brand_name; });
+      found[0].quantity -= 1;
+      found[0].total_amt = found[0].quantity * product.price;
     }
      
     if (product.totalSelectedItems === 0) {
       product.isHidden = false;
+      $scope.allGoods = ($scope.allGoods).filter(function(item) { return item.name !== product.brand_name; });
     }
   };
 
