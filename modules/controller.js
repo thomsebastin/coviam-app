8ccad1f0c62a13c0a1cd632c96c5b21c2d69e6e0
Thomas Sebastian <thomas@inkoniq.com>
2016-06-22 20:37:53 +0530

    add to cart click functionality and json integration

--- modules/controller.js
+++ modules/controller.js
@@ -3,14 +3,22 @@ var cvController = angular.module('cvApp.controller', []);
 cvController.controller('cvCtrl', function($scope, GlobalService) {
   console.log("Controller inititated!!");
   
-  $scope.isHidden = true;
+  //$scope.isHidden = true;
+  $scope.itemsInCart = 0;
 
   // calling API for getting products
   $scope.getProductDetails = function() {
     GlobalService.getProductDetails().then(function(res) {
       console.log(res);
+      $scope.allProducts = res.data;
+      console.log($scope.allProducts);
     })
   };
+
+  // triggers when add to cart is clicked
+  $scope.addToCart = function(product) {
+    product.isHidden = true;
+  }
 
   $scope.getProductDetails();
 });
